6e8318e938b7f6cf4e7e6b39cb8cf48dea6291bc
Ovinkin Kirill <103418173+Randosky@users.noreply.github.com>
2023-11-16 00:43:10 +0500

    Create layout + styles Contacts Section

--- Scripts/yandexMap.js
+++ Scripts/yandexMap.js
@@ -1,19 +1,31 @@
 async function initMap() {
     await ymaps3.ready;
 
-    const {YMap, YMapDefaultSchemeLayer} = ymaps3;
+    const {YMap, YMapDefaultSchemeLayer, YMapDefaultFeaturesLayer, YMapMarker} = ymaps3;
 
     const map = new YMap(
         document.getElementById('map'),
         {
             location: {
-                center: [37.62209300000001, 55.75554289958026],
+                center: [36.61986834792114,55.89836380661811],
                 zoom: 7
             }
         }
     );
 
+
+    const markerElement = document.getElementById("mapMarker");
+
+    const marker = new YMapMarker(
+        {
+            coordinates: [37.41986834792114,56.29836380661811],
+        },
+        markerElement
+    );
+
     map.addChild(new YMapDefaultSchemeLayer());
+    map.addChild(new YMapDefaultFeaturesLayer());
+    map.addChild(marker);
 }
 
 initMap().then();
