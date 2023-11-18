6968de92d700a46825a7044612a82b538d7ddea1
Ovinkin Kirill <103418173+Randosky@users.noreply.github.com>
2023-11-18 14:36:31 +0500

    Feedback corrections

--- Scripts/swiper.js
+++ Scripts/swiper.js
@@ -1,40 +1,65 @@
 const officeSwiper = new Swiper(".office__swiper", {
-    slidesPerView: 3,
-    spaceBetween: 30,
+    slidesPerView: 1,
+    pagination: {
+        el: ".office__progressbar",
+        type: "progressbar"
+    },
     navigation: {
         nextEl: '.office__btns-next',
         prevEl: '.office__btns-prev',
     },
-    renderProgressbar: (progressbarFillClass) => {
-        return '<span class="' + progressbarFillClass + '"></span>';
+    breakpoints: {
+        // when window width is >= 1470px
+        1470: {
+            slidesPerView: 3,
+            spaceBetween: 40,
+        },
+        1050: {
+            slidesPerView: 3,
+            spaceBetween: 24,
+        },
+        769: {
+            slidesPerView: 2,
+            spaceBetween: 24,
+        }
     }
 });
 
 const teamSwiper = new Swiper(".team__swiper", {
-    slidesPerView: 3,
-    spaceBetween: 30,
+    slidesPerView: 1,
+    spaceBetween: 24,
+    pagination: {
+        el: ".team__progressbar",
+        type: "progressbar"
+    },
     navigation: {
         nextEl: '.team__btns-next',
         prevEl: '.team__btns-prev',
     },
     breakpoints: {
-        // when window width is >= 320px
-        320: {
-            slidesPerView: 2,
-            spaceBetween: 20
+        // when window width is >= 1470px
+        1800: {
+            slidesPerView: 4,
+            spaceBetween: 40,
+            grid: {
+                rows: 1,
+            },
         },
-        // when window width is >= 480px
-        480: {
+        1470: {
+            slidesPerView: 3,
+            spaceBetween: 24,
+            grid: {
+                rows: 1,
+            },
+        },
+        1050: {
+            slidesPerView: 2,
+            spaceBetween: 24,
+            slidesPerColumn: 2,
             grid: {
                 rows: 2,
+                fill: "row"
             },
-            slidesPerView: 3,
-            spaceBetween: 30
         },
-        // when window width is >= 640px
-        640: {
-            slidesPerView: 3,
-            spaceBetween: 40
-        }
     }
 });
